a7c3d3b8e0cc11d0c107f20bd8ed19778abbbc44
ZoeYUU <yuxu@tcd.ie>
2018-04-18 09:26:22 +0100

    GL2

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour {
+
+    public GameObject Smile;
+    public GameObject Goo;
+    public GameObject Good;
+    public GameObject Lu;
+    public GameObject Luck;
+
+	// Use this for initialization
+	void Start () {
+
+        Smile.SetActive(true);
+        Goo.SetActive(false);
+        Good.SetActive(false);
+        Lu.SetActive(false);
+        Luck.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Smile.SetActive(true);
+            Goo.SetActive(false);
+            Good.SetActive(false);
+            Lu.SetActive(false);
+            Luck.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Smile.SetActive(false);
+            Goo.SetActive(true);
+            Good.SetActive(false);
+            Lu.SetActive(false);
+            Luck.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Smile.SetActive(false);
+            Goo.SetActive(false);
+            Good.SetActive(true);
+            Lu.SetActive(false);
+            Luck.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Smile.SetActive(false);
+            Goo.SetActive(false);
+            Good.SetActive(false);
+            Lu.SetActive(true);
+            Luck.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Smile.SetActive(false);
+            Goo.SetActive(false);
+            Good.SetActive(false);
+            Lu.SetActive(false);
+            Luck.SetActive(true);
+        }
+
+    }
+}
